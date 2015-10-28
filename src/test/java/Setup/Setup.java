46eaf4a86444dbbf334a6577587d24062f2466ad
olenaostapchuk <eostapchuk@anotheria.net>
2015-10-28 20:59:15 +0200

    ChromeDrivedService

--- src/test/java/Setup/Setup.java
+++ src/test/java/Setup/Setup.java
@@ -1,7 +1,13 @@
 package Setup;
 
+import com.google.common.collect.ImmutableMap;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.IOException;
 
 /**
  * Created by olenaostapchuk on 19.10.15.
@@ -9,13 +15,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Setup {
     static WebDriver driver;
 
-    public static WebDriver getDriver(){
+   /* public static WebDriver getDriver(){
         if (driver==null){
             System.setProperty("webdriver.chrome.driver", "/Users/olenaostapchuk/Documents/Selenium/chromedriver");
             driver = new ChromeDriver();
 
         }
 
+        return driver;
+    }*/
+
+    public static WebDriver getDriver(){
+        //Change system property of webdriver manager to download latest 2.9 version of chromedriver
+        System.setProperty("wdm.chromeDriverVersion", "2.9");
+        System.out.println("Getting the driver instance");
+        System.setProperty("webdriver.chrome.driver", "/Users/olenaostapchuk/Documents/Selenium/chromedriver");
+        if (driver == null) {
+            System.out.println("Starting a service...");
+            ChromeDriverService service = new ChromeDriverService.Builder()
+                    .withEnvironment(ImmutableMap.of("DISPLAY", ":20"))
+                    .usingAnyFreePort()
+                    .build();
+            try {
+                service.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            driver = new RemoteWebDriver(service.getUrl(),
+                    DesiredCapabilities.chrome());
+        }
         return driver;
     }
 }
