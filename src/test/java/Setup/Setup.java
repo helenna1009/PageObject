6f7772357808e5f706413576d8a75a957542b2fe
olenaostapchuk <eostapchuk@anotheria.net>
2015-11-02 13:20:40 +0200

    Chrome Driver update

--- src/test/java/Setup/Setup.java
+++ src/test/java/Setup/Setup.java
@@ -29,7 +29,7 @@ public class Setup {
         //Change system property of webdriver manager to download latest 2.9 version of chromedriver
       //  System.setProperty("wdm.chromeDriverVersion", "2.9");
 
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/share/chromedriver");
         System.out.println("Getting the driver instance");
         if (driver == null) {
             System.out.println("Starting a service...");
